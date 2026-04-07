3c6795212934301473df88e832d04213c7ca3b9e
Samit Mohan <samitmohan@gmail.com>
2026-04-08 05:23:19 +0530

    test: update small_config for MoE fields, add is_residual test

--- tests/test_gpt.py
+++ tests/test_gpt.py
@@ -12,6 +12,8 @@ def small_config(**kwargs):
         n_head=4,
         n_kv_head=2,
         n_embd=128,
+        use_moe=False,
+        n_experts=4,
     )
     defaults.update(kwargs)
     return GPTConfig(**defaults)
@@ -29,3 +31,9 @@ def test_mlp_hidden_dim_multiple_of_64():
     config = small_config(n_embd=128)
     mlp = MLP(config)
     assert mlp.fc1.out_features % 64 == 0
+
+
+def test_mlp_is_residual_marked():
+    config = small_config()
+    mlp = MLP(config)
+    assert getattr(mlp.fc2, "_is_residual", False) is True
